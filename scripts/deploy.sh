4562c9093b27f1340cd4139f843603308a43ab1c
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-03-19 22:53:27 +0100

    fix CI config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,12 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-[ -z "$SUPER_AWS_CLOUD_FRONT_DISTRIBUTION_ID" ] && echo "Need to set \$SUPER_AWS_CLOUD_FRONT_DISTRIBUTION_ID" && exit 1;
+[ -z "$AWS_CLOUD_FRONT_DISTRIBUTION_ID" ] && echo "Need to set \$AWS_CLOUD_FRONT_DISTRIBUTION_ID" && exit 1;
 
 echo "Uploading new files to s3"
-aws s3 sync static s3://super-ci-homepage
+aws s3 sync static s3://codechecks-landing-bucket
 
 echo "Invalidating Cloudfront cache"
-aws cloudfront create-invalidation --distribution-id $SUPER_AWS_CLOUD_FRONT_DISTRIBUTION_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id $AWS_CLOUD_FRONT_DISTRIBUTION_ID --paths "/*"
 
 echo "done."
